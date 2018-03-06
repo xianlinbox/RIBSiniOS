2da4811b445c7ee2a1aab4e97768c25635787801
xianlinbox <xianlinbox@gmail.com>
2018-03-06 10:34:35 -0600

    add LoggedOutRIB

---
+++ TicTacToe/LoggedOut/LoggedOutInteractor.swift
@@ -0,0 +1,53 @@
+//
+//  LoggedOutInteractor.swift
+//  TicTacToe
+//
+//  Created by Xianning Liu  on 06/03/2018.
+//  Copyright © 2018 Uber. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+
+protocol LoggedOutRouting: ViewableRouting {
+    // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
+}
+
+protocol LoggedOutPresentable: Presentable {
+    weak var listener: LoggedOutPresentableListener? { get set }
+    // TODO: Declare methods the interactor can invoke the presenter to present data.
+}
+
+protocol LoggedOutListener: class {
+    // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+}
+
+final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, LoggedOutInteractable, LoggedOutPresentableListener {
+
+    weak var router: LoggedOutRouting?
+    weak var listener: LoggedOutListener?
+
+    // TODO: Add additional dependencies to constructor. Do not perform any logic
+    // in constructor.
+    override init(presenter: LoggedOutPresentable) {
+        super.init(presenter: presenter)
+        presenter.listener = self
+    }
+
+    override func didBecomeActive() {
+        super.didBecomeActive()
+        // TODO: Implement business logic here.
+    }
+
+    override func willResignActive() {
+        super.willResignActive()
+        // TODO: Pause any business logic.
+    }
+  
+  func login(withPlayer1Name player1Name: String?, withPlayer2Name player2Name: String?) {
+    let p1 = player1Name ?? "Player 1"
+    let p2 = player2Name ?? "Player 2"
+    
+    print("\(p1) vs \(p2)")
+  }
+}
---
+++ TicTacToe/LoggedOut/LoggedOutRouter.swift
@@ -0,0 +1,27 @@
+//
+//  LoggedOutRouter.swift
+//  TicTacToe
+//
+//  Created by Xianning Liu  on 06/03/2018.
+//  Copyright © 2018 Uber. All rights reserved.
+//
+
+import RIBs
+
+protocol LoggedOutInteractable: Interactable {
+    weak var router: LoggedOutRouting? { get set }
+    weak var listener: LoggedOutListener? { get set }
+}
+
+protocol LoggedOutViewControllable: ViewControllable {
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+}
+
+final class LoggedOutRouter: ViewableRouter<LoggedOutInteractable, LoggedOutViewControllable>, LoggedOutRouting {
+
+    // TODO: Constructor inject child builder protocols to allow building children.
+    override init(interactor: LoggedOutInteractable, viewController: LoggedOutViewControllable) {
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+}
