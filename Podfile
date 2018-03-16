05331984e7c4d2f519056e616e272c36b7cd8661
xianlinbox <xianlinbox@gmail.com>
2018-03-16 16:34:35 -0500

    add test for RootRouter

--- Podfile
+++ Podfile
@@ -7,9 +7,12 @@ target 'TicTacToe' do
   pod 'RIBs'
   pod 'SnapKit', '~> 4.0.0'
   pod 'RxCocoa', '~> 4.0.0'
+  
+  target 'TicTacToeTests' do
+    inherit! :search_paths
+    pod 'Quick'
+    pod 'Nimble'
+  end
 end
 
-target 'TicTacToeTests' do
-  pod 'Quick'
-  pod 'Nimble'
-end
+
--- TicTacToeTests/Root/RootRouterTests.swift
+++ TicTacToeTests/Root/RootRouterTests.swift
@@ -8,16 +8,40 @@
 
 @testable import TicTacToe
 import XCTest
+import Quick
+import Nimble
 
 final class RootRouterTests: XCTestCase {
-    private var router: RootRouter!
-
-    override func setUp() {
-        super.setUp()
+  private var router: RootRouter!
+  private var loggedInBuilder: LoggedInBuildableMock!
+  private var rootInteractor: RootInteractableMock!
+  
+  override func setUp() {
+    super.setUp()
+    rootInteractor = RootInteractableMock()
+    let viewController = RootViewControllableMock()
+    loggedInBuilder = LoggedInBuildableMock()
+    let loggedOutBuilder = LoggedOutBuildableMock()
+    router = RootRouter(interactor: rootInteractor, viewController: viewController, loggedOutBuilder: loggedOutBuilder, loggedInBuilder: loggedInBuilder)
+  }
+  
+  func test_routeToLoggedIn_invokesToExampleResult() {
+    let loggedInRouter = LoggedInRoutingMock(interactable: LoggedInInteractableMock())
+    var assignedListener: LoggedInListener? = nil
+    loggedInBuilder.buildHandler = { (_ listener: LoggedInListener) -> (LoggedInRouting) in
+      assignedListener = listener
+      return loggedInRouter
     }
-
-    func test_routeToExample_invokesToExampleResult() {
-        // This is an example of a router test case.
-        // Test your router functions invokes the corresponding builder, attachesChild, presents VC, etc.
-    }
+    
+    XCTAssertNil(assignedListener)
+    XCTAssertEqual(loggedInBuilder.buildCallCount, 0)
+    XCTAssertEqual(loggedInRouter.loadCallCount, 0)
+    
+    router.routeToLoggedIn(withPlayer1: "1", player2: "2")
+    
+    XCTAssertTrue(assignedListener === rootInteractor)
+    XCTAssertEqual(loggedInBuilder.buildCallCount, 1)
+    XCTAssertEqual(loggedInRouter.loadCallCount, 1)
+  }
 }
+
